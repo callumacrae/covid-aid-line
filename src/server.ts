ec1a35c8e6d7f9986f3c9590424a9e01fb7c4e46
Callum Macrae <callum@macr.ae>
2020-03-26 17:12:29 +0000

    Remove test code

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,6 @@ import bodyParser from 'body-parser';
 import incomingCall from '../api/incoming-call';
 import incomingCallAction from '../api/incoming-call-action';
 import outgoingCall from '../api/outgoing-call';
-import test from '../api/test';
 
 const app = express();
 const port = 3000;
@@ -14,6 +13,5 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/incoming-call', incomingCall);
 app.post('/incoming-call-action', incomingCallAction);
 app.post('/outgoing-call/:sid', outgoingCall);
-app.get('/test', test);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
